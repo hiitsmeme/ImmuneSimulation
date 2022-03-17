2a628d3dd6ab4a578dc27f7b01877363e0cbf9f4
hiitsmeme <sonjatopf@gmx.at>
2022-03-17 21:26:21 +0100

    window, macrophage class

---
+++ Client/Marcophage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Raylib_cs;
+using System.Numerics;
+
+namespace Client
+{
+    public class Macrophage
+    {
+        // screen size
+        private int screenWidth;
+        private int screenHeight;
+
+        // ball properties
+        private float posx;
+        private float posy;
+        private int radius = 10;
+        private int updateFactor;
+        public Vector2 position;
+
+        // constructor
+        public Macrophage(int screenWidth, int screenHeight, int updateFactor)
+        {
+            // set screenHeight
+            this.screenWidth = screenWidth;
+            this.screenHeight = screenHeight;
+
+            this.updateFactor = updateFactor;
+
+            posx = (float) screenWidth / 2;
+            posy = (float) screenHeight / 2;
+        }
+
+        public void updatePosition()
+        {
+            // get probs for updating
+            Random rnd = new Random();
+            int probx = rnd.Next(0, 101);
+            int proby = rnd.Next(0, 101);
+
+            if ((posx + updateFactor) <= screenWidth - radius && probx <= 50)
+            {
+                posx += updateFactor;
+            }
+            else if (posx - updateFactor >= radius)
+            {
+                posx -= updateFactor;
+            }
+            // y up or down
+            if ((posy + updateFactor) <= screenHeight - radius && proby <= 50)
+            {
+                posy += updateFactor;
+            }
+            else if (posy - updateFactor >= radius)
+            {
+                posy -= updateFactor;
+            }
+
+            position.X = posx;
+            position.Y = posy;
+        }
+
+        public void draw()
+        {
+            Raylib.DrawCircleV(position, radius, Color.VIOLET);
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using Raylib_cs;
+using Client;
 
-namespace Client
+
+Random rnd = new Random();
+
+
+// set height and width of win
+int screenWidth = 800;
+int screenHeight = 800;
+
+int updateFactor = 1;
+
+// create ball instances
+Macrophage macro1 = new Macrophage(screenWidth, screenHeight, updateFactor);
+Macrophage macro2 = new Macrophage(screenWidth, screenHeight, updateFactor);
+Macrophage macro3 = new Macrophage(screenWidth, screenHeight, updateFactor);
+Macrophage macro4 = new Macrophage(screenWidth, screenHeight, updateFactor);
+
+
+Raylib.InitWindow(screenWidth, screenHeight, "Immune Simulation");
+
+Raylib.SetTargetFPS(30);
+
+// main game loop
+while (!Raylib.WindowShouldClose())
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    // Update
+    // update ball position
+    macro1.updatePosition();
+    macro2.updatePosition();
+    macro3.updatePosition();
+    macro4.updatePosition();
+
+    // Draw
+    Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.WHITE);
+
+        macro1.draw();
+        macro2.draw();
+        macro3.draw();
+        macro4.draw();
+
+    Raylib.EndDrawing();
 }
+
+Raylib.CloseWindow();
+
+return 0;
